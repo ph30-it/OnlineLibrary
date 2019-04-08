057ddcb6a12a3cfc0d2b10a42d1ffa30c9b61797
TanToan99 <tantoan1909@gmail.com>
2019-04-08 20:57:45 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('firstname');
             $table->string('lastname');
             $table->string('image')->nullable();
+            $table->dateTime('account_expiry_date');
             $table->integer('roles')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
